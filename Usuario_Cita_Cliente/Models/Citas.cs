9feb8449aaa02b0627bd957e2cf4fc3fea8c01fc
ErickVO <59853887+ErickVO@users.noreply.github.com>
2021-10-09 19:42:01 -0400

    Cambios en el sistema

---
+++ Usuario_Cita_Cliente/Models/Citas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroCita_Cliente.Models
+{
+  public class Citas
+    {
+     [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+     public int CitaId { get; set; }
+     [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+     public string Descripcion { get; set; }
+     [Required(ErrorMessage = "Este campo no puede estar vacio.")]
+     public DateTime Fecha_Cita { get; set; }
+
+
+      public Citas()
+      {
+        CitaId = 0;
+        Descripcion = string.Empty;
+        Fecha_Cita = DateTime.Now;
+        
+      }
+        
+  }
+}
